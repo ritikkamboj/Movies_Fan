e5edcc249c133990ac64dddfd9dd278f9eb92d93
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-03-11 17:43:37 +0530

    made the use of useContext in Favourite components

--- src/pages/Favourite.jsx
+++ src/pages/Favourite.jsx
@@ -1,12 +1,47 @@
 import React from "react";
+import { useMoviesContext } from "../MoviesContext/MovieContext";
+import MovieCard from "../components/MovieCard";
 
 function Favourite() {
+  const { favourites } = useMoviesContext();
+
+  if (favourites.length !== 0) {
+    console.log(favourites.length);
+    return (
+      <div>
+        <h2>Below are your Favorites Cinema masterpiece</h2>
+        <div className="">
+          {favourites.map((movie) => (
+            <MovieCard movie={movie} key={movie.id} />
+          ))}
+        </div>
+      </div>
+    );
+  }
   return (
     <div>
       <h3>No data found Here </h3>
       <p>Type something in search bar so that you can get results </p>
     </div>
   );
+
+  // {
+  //   favourites ? (
+  //     <div>
+  //       <h2>Below are your Favorites Cinema masterpiece</h2>
+  //       <div className="">
+  //         {favourites.map((movie) => (
+  //           <MovieCard movie={movie} key={movie.id} />
+  //         ))}
+  //       </div>
+  //     </div>
+  //   ) : (
+  //     <div>
+  //       <h3>No data found Here </h3>
+  //       <p>Type something in search bar so that you can get results </p>
+  //     </div>
+  //   );
+  // }
 }
 
 export default Favourite;
